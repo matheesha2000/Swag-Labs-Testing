e8772864f130bf555b4ae65acb758a54ef3b54a4
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-05-28 05:07:10 +0530

    feat: user login supportive method created

--- src/test/java/TestNG/SwagLabsWebTest.java
+++ src/test/java/TestNG/SwagLabsWebTest.java
@@ -1,5 +1,6 @@
 package TestNG;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -20,6 +21,42 @@ public class SwagLabsWebTest {
         driver.manage().window().maximize();
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Supportive methods
+    public void userLogin() throws InterruptedException {
+
+        driver.get(BaseURL);
+        Thread.sleep(3000);
+
+        //Identify the Username text and send value
+        driver.findElement(By.name("username")).sendKeys("standard_user");
+        Thread.sleep(1000);
+
+        //Identify the Password txt and send value
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        Thread.sleep(1000);
+
+        //Clicked the login button
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        Thread.sleep(3000);
+    }
+
 
 
 }
