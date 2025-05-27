f43476ff9f8a2de37bd8a2578137172e8f7c0270
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-05-28 04:11:21 +0530

    feat: global variable section created

---
+++ src/test/java/TestNG/SwagLabsWebTest.java
@@ -0,0 +1,16 @@
+package TestNG;
+
+import org.openqa.selenium.WebDriver;
+
+public class SwagLabsWebTest {
+
+    //Global Variable Section
+
+    String BaseURL = "https://www.saucedemo.com/";
+    WebDriver driver;
+    String actualText;
+    String expectedText;
+    Boolean status;
+
+
+}
