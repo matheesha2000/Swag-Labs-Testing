61de8fc60aab3402ef4b7486cf20be2afd358797
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-05-29 19:02:24 +0530

    feat: load product page test case created

--- src/test/java/TestNG/SwagLabsWebTest.java
+++ src/test/java/TestNG/SwagLabsWebTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class SwagLabsWebTest {
 
@@ -15,12 +16,51 @@ public class SwagLabsWebTest {
     String expectedText;
     Boolean status;
 
+    //Before Test Section
     @BeforeTest
     public void BeforeTestMethod() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
     }
+
+    //Test Case Section
+
+    //Test Case : 001
+    @Test(priority = 1)
+    public void loadProductPage() throws InterruptedException {
+
+        System.out.print("\n----------------------|| Swag_Labs_Test || -----------------------------------------\n\n");
+        System.out.println("================================= Test 001============================================\n");
+        System.out.println("LOAD PRODUCT PAGE TEST CASE\n");
+
+        //Login to the website
+        userLogin();
+
+        //Verify the correct page
+        expectedText = "Products";
+        actualText = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).getText();
+
+        //print actual text
+        System.out.println("Test Case 001 Actual Text:"+ actualText);
+
+        if (expectedText.equals(actualText)) {
+
+            System.out.println("TC 001: PASS");
+            System.out.println("Login Success, Load the Product Page\n");
+        }
+        else {
+            System.out.println("TC 001: FAIL");
+            System.out.println("Login Failure, Can't Load the Product Page\n");
+        }
+
+        System.out.println("----------------------------------------------------------------------------------------");
+
+        Thread.sleep(5000);
+
+    }
+
+
 
 
 
@@ -45,7 +85,7 @@ public class SwagLabsWebTest {
         Thread.sleep(3000);
 
         //Identify the Username text and send value
-        driver.findElement(By.name("username")).sendKeys("standard_user");
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
         Thread.sleep(1000);
 
         //Identify the Password txt and send value
